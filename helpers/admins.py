7d9917253917f82f13719efb575a22bd4b43f77a
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-07-30 07:25:57 +0530

    Update admins.py

--- helpers/admins.py
+++ helpers/admins.py
@@ -1,13 +1,12 @@
-
 from typing import List
 
-from pyrogram.types import Chat, User
+from pyrogram.types import Chat
 
-from cache.admins import get as gett
-from cache.admins import set
+from function.admins import get as gett
+from function.admins import set
 
 
-async def get_administrators(chat: Chat) -> List[User]:
+async def get_administrators(chat: Chat) -> List[int]:
     get = gett(chat.id)
 
     if get:
@@ -17,8 +16,8 @@ async def get_administrators(chat: Chat) -> List[User]:
         to_set = []
 
         for administrator in administrators:
-            # if administrator.can_manage_voice_chats:
-            to_set.append(administrator.user.id)
+            if administrator.can_manage_voice_chats:
+                to_set.append(administrator.user.id)
 
         set(chat.id, to_set)
         return await get_administrators(chat)
