f75b72596949a87831cf7b379ca0b7696070b7dd
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-07-30 07:11:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from pyrogram import Client as Bot
 
-from DaisyXMusic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from DaisyXMusic.services.callsmusic import run
+from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -14,7 +14,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="modules"),
+    plugins=dict(root="DaisyXMusic.modules"),
 )
 
 bot.start()
