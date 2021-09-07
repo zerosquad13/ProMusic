ed5265778450d34f0b0024fcb5de9c26c7bdb903
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-09-07 09:23:59 +0530

    Update ping.py

--- modules/ping.py
+++ modules/ping.py
@@ -40,3 +40,15 @@ async def ping_pong(client: Client, message: Message):
         "🏓 `PONG!!`\n"
         f"⚡️ `{delta_ping * 1000:.3f} ms`"
     )
+
+
+@Client.on_message(command(["uptime", f"uptime@{BOT_USERNAME}"]) & ~filters.edited)
+async def get_uptime(client: Client, message: Message):
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    await message.reply_text(
+        "🤖 bot status:\n"
+        f"• **uptime:** `{uptime}`\n"
+        f"• **start time:** `{START_TIME_ISO}`"
+    )
