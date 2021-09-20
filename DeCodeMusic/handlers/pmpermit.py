ff4b3cff03da247a60ab3ee6d0863ad062d6e1a4
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-20 13:17:04 +0530

    Update pmpermit.py

--- DeCodeMusic/handlers/pmpermit.py
+++ DeCodeMusic/handlers/pmpermit.py
@@ -3,7 +3,7 @@ import asyncio
 from config import SUDO_USERS, PMPERMIT, OWNER_USERNAME, BOT_NAME, UPDATES_CHANNEL, GROUP_SUPPORT
 from pyrogram import filters
 from pyrogram.types import Message
-from callsmusic.callsmusic import client as USER
+from DeCodeMusic.callsmusic.callsmusic import client as USER
 
 PMSET =True
 pchats = []
