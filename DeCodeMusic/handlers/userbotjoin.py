d91c0863832596046cec38ad1da48182c80df807
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-20 13:37:57 +0530

    Update userbotjoin.py

--- DeCodeMusic/handlers/userbotjoin.py
+++ DeCodeMusic/handlers/userbotjoin.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from pyrogram.errors import UserAlreadyParticipant
 from DeCodeMusic.helpers.filters import command
 from DeCodeMusic.helpers.decorators import authorized_users_only, errors
-from callsmusic.callsmusic import client as USER
+from DeCodeMusic.callsmusic.callsmusic import client as USER
 from config import BOT_USERNAME, SUDO_USERS
 
 
