849548ddb6dca3ff01541e29e7185ca36e5f8ed6
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-20 13:48:30 +0530

    Update broadcast.py

--- DeCodeMusic/handlers/broadcast.py
+++ DeCodeMusic/handlers/broadcast.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Dialog, Chat, Message
 from pyrogram.errors import UserAlreadyParticipant
 
-from callsmusic.callsmusic import client as veez
-from config import SUDO_USERS
+from DeCodeMusic.callsmusic.callsmusic import client as veez
+from DeCodeMusic.config import SUDO_USERS
 
 @Client.on_message(filters.command(["gcast"]))
 async def broadcast(_, message: Message):
