757c25fed64523bdd6850e4ac357ee24e0177054
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-20 13:10:28 +0530

    Update ytsearch.py

--- DeCodeMusic/handlers/ytsearch.py
+++ DeCodeMusic/handlers/ytsearch.py
@@ -2,7 +2,7 @@ import logging
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
 from pyrogram import Client as app
-from helpers.filters import command
+from DeCodeMusic.helpers.filters import command
 from DeCodeMusic.config import BOT_USERNAME
 
 
@@ -23,10 +23,10 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 5:
-            text += f"**Judul:** `{results[i]['title']}`\n"
-            text += f"**Durasi:** {results[i]['duration']}\n"
-            text += f"**Views:** {results[i]['views']}\n"
-            text += f"**Channel:** {results[i]['channel']}\n"
+            text += f"**🅝🅐🅜🅔:** `{results[i]['title']}`\n"
+            text += f"**🅓🅤🅡🅐🅣🅘🅞🅝:** {results[i]['duration']}\n"
+            text += f"**🅥🅘🅔🅦🅢:** {results[i]['views']}\n"
+            text += f"**🅒🅗🅐🅝🅝🅔🅛:** {results[i]['channel']}\n"
             text += f"https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
