7d0bb580075940d314bee12f268fa04471a9c94e
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-20 13:21:52 +0530

    Update entertainment.py

--- DeCodeMusic/handlers/entertainment.py
+++ DeCodeMusic/handlers/entertainment.py
@@ -5,7 +5,7 @@
 import requests
 from pyrogram import Client
 from config import BOT_USERNAME
-from helpers.filters import command
+from DeCodeMusic.helpers.filters import command
 
 @Client.on_message(command(["asupan", f"asupan@{BOT_USERNAME}"]))
 async def asupan(client, message):
