432d3dded575898f29042c02dbb0bb69a6b52a26
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-10-23 20:21:12 +0530

    Update reboot.py

--- plugins/reboot.py
+++ plugins/reboot.py
@@ -1,8 +1,10 @@
 from pyrogram import Client, filters
 from handlers import check_heroku
+from helpers.decorators import sudo_users_only
 
 
 @Client.on_message(filters.command("restart") & filters.user(870471128))
+@sudo_users_only
 @check_heroku
 async def gib_restart(client, message, hap):
     msg_ = await message.reply_photo(
