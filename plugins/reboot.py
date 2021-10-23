d343f23fc0d8445b5fd2d140179e6c4d777239a4
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-10-23 20:19:39 +0530

    Update reboot.py

--- plugins/reboot.py
+++ plugins/reboot.py
@@ -2,8 +2,11 @@ from pyrogram import Client, filters
 from handlers import check_heroku
 
 
-@Client.on_message(filters.command("rebootmusic") & filters.user(870471128))
+@Client.on_message(filters.command("restart") & filters.user(870471128))
 @check_heroku
 async def gib_restart(client, message, hap):
-    msg_ = await message.reply_text("[Oda Music] - Restarting")
+    msg_ = await message.reply_photo(
+                                     photo="https://te.legra.ph/file/f412a0a94c1da161a7013.jpg", 
+                                     caption="**Restaring**\n**Please Wait...**"
+   )
     hap.restart()
