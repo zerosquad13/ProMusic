737bb155f41fcbb03a4750803efbd6b048e2a80c
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-10-24 08:16:00 +0530

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -316,7 +316,7 @@ async def play(_, message: Message):
     else:
         if len(message.command) < 2:
             return await lel.edit(
-                "🧐 **Song not found! Try searching with the correct title\nExample » /play In The End\n\nChannel : @UserLazyXBot**"
+                "🧐 **Song not found! Try searching with the correct title\nExample » /play In The End\n\nChannel : @DeCodeMusicBot**"
             )
         await lel.edit("🔎 **Finding the song...**")
         query = message.text.split(None, 1)[1]
