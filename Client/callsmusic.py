1b1c3e212805c1f9b39d1e2cb3280dc91d2e063f
BlazeOp <94062864+DeCodeOwner@users.noreply.github.com>
2021-11-27 00:33:30 +0530

    Umm Fixed Queue

--- Client/callsmusic.py
+++ Client/callsmusic.py
@@ -1,6 +1,7 @@
 from pyrogram import Client
 from pytgcalls import PyTgCalls
 from pytgcalls.types import Update
+from pytgcalls.types.input_stream import InputStream
 from pytgcalls.types.input_stream import InputAudioStream
 
 from config import API_HASH, API_ID, SESSION_NAME
@@ -20,11 +21,14 @@ async def on_stream_end(client: PyTgCalls, update: Update) -> None:
         await pytgcalls.leave_group_call(chat_id)
     else:
         await pytgcalls.change_stream(
-            chat_id, 
-            InputAudioStream(
-                queues.get(chat_id)["file"],
-            ),
-        )
+                chat_id, 
+                InputStream(
+                    InputAudioStream(
+                        callsmusic.queues.get(chat_id)["file"],
+                    ),
+                ),
+            )
+      
 
 
 run = pytgcalls.start
