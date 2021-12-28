0b829d10a8d0d464a994e27e3c210108a7e664d6
eVEXERA <96389440+eVEXERA@users.noreply.github.com>
2021-12-28 22:07:57 +0530

    Create admins.py

---
+++ ProMusic/admins.py
@@ -0,0 +1,207 @@
+from asyncio import QueueEmpty
+
+from callsmusic import callsmusic
+from callsmusic.queues import queues
+from config import BOT_USERNAME, que
+from DeCoDe.cache.admins import admins
+from helpers.channelmusic import get_chat_id
+from helpers.dbtools import delcmd_is_on, delcmd_off, delcmd_on
+from helpers.decorators import authorized_users_only, errors
+from helpers.filters import command, other_filters
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputStream
+from pyrogram import Client, filters
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+)
+
+ACTV_CALLS = []
+
+
+# @Client.on_message(filters.text & ~filters.private)
+# async def delcmd(_, message: Message):
+#    if await delcmd_is_on(message.chat.id) and message.text.startswith("/") or message.text.startswith("!") or message.text.startswith("."):
+#        await message.delete()
+#    await message.continue_propagation()
+
+# remove the ( # ) if you want the auto del cmd feature is on
+
+
+@Client.on_message(command(["reload", f"reload@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def update_admin(client, message):
+    global admins
+    new_admins = []
+    new_ads = await client.get_chat_members(message.chat.id, filter="administrators")
+    for u in new_ads:
+        new_admins.append(u.user.id)
+    admins[message.chat.id] = new_admins
+    await client.send_message(
+        message.chat.id,
+        "✅ Bot **reloaded correctly !**\n\n• The **Admin list** has been **updated !**"
+    )
+
+
+@Client.on_message(command(["pause", f"pause@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❌ **no music is currently playing**")
+    else:
+        await callsmusic.pytgcalls.pause_stream(chat_id)
+        await _.send_message(
+            message.chat.id,
+            "⏸ **Track paused.**\n\n• **To resume the playback, use the**\n» /resume command."
+        )
+
+
+@Client.on_message(command(["resume", f"resume@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❌ **no music is paused**")
+    else:
+        await callsmusic.pytgcalls.resume_stream(chat_id)
+        await _.send_message(
+            message.chat.id,
+            "▶️ **Track resumed.**\n\n• **To pause the playback, use the**\n» /pause command."
+        )
+
+
+@Client.on_message(command(["end", f"end@{BOT_USERNAME}", "stop", f"end@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    chat_id = get_chat_id(message.chat)
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❌ **no music is currently playing**")
+    else:
+        try:
+            queues.clear(chat_id)
+        except QueueEmpty:
+            pass
+        await callsmusic.pytgcalls.leave_group_call(chat_id)
+        await _.send_message(
+            message.chat.id,
+            "✅ __The userbot has disconnected from voice chat.__"
+        )
+
+
+@Client.on_message(command(["skip", f"skip@{BOT_USERNAME}", "next", f"next@{BOT_USERNAME}"]) & other_filters)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    global que
+    chat_id = message.chat.id
+    for x in callsmusic.pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❌ **no music is currently playing**")
+    else:
+        queues.task_done(chat_id)
+        
+        if queues.is_empty(chat_id):
+            await callsmusic.pytgcalls.leave_group_call(chat_id)
+        else:
+            await callsmusic.pytgcalls.change_stream(
+                chat_id, 
+                InputStream(
+                    InputAudioStream(
+                        callsmusic.queues.get(chat_id)["file"],
+                    ),
+                ),
+            )
+                
+    qeue = que.get(chat_id)
+    if qeue:
+        qeue.pop(0)
+    if not qeue:
+        return
+    await _.send_message(
+        message.chat.id,
+        "⏭ **You've skipped to the next song.**"
+    )
+
+
+@Client.on_message(command(["auth", f"auth@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def authenticate(client, message):
+    global admins
+    if not message.reply_to_message:
+        return await message.reply("💡 reply to message to authorize user !")
+    if message.reply_to_message.from_user.id not in admins[message.chat.id]:
+        new_admins = admins[message.chat.id]
+        new_admins.append(message.reply_to_message.from_user.id)
+        admins[message.chat.id] = new_admins
+        await message.reply(
+            "🟢 user authorized.\n\nfrom now on, that's user can use the admin commands."
+        )
+    else:
+        await message.reply("✅ user already authorized!")
+
+
+@Client.on_message(command(["unauth", f"deauth@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def deautenticate(client, message):
+    global admins
+    if not message.reply_to_message:
+        return await message.reply("💡 reply to message to deauthorize user !")
+    if message.reply_to_message.from_user.id in admins[message.chat.id]:
+        new_admins = admins[message.chat.id]
+        new_admins.remove(message.reply_to_message.from_user.id)
+        admins[message.chat.id] = new_admins
+        await message.reply(
+            "🔴 user deauthorized.\n\nfrom now that's user can't use the admin commands."
+        )
+    else:
+        await message.reply("✅ user already deauthorized!")
+
+
+# this is a anti cmd feature
+@Client.on_message(command(["delcmd", f"delcmd@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def delcmdc(_, message: Message):
+    if len(message.command) != 2:
+        return await message.reply_text(
+            "read the /help message to know how to use this command"
+        )
+    status = message.text.split(None, 1)[1].strip()
+    status = status.lower()
+    chat_id = message.chat.id
+    if status == "on":
+        if await delcmd_is_on(message.chat.id):
+            return await message.reply_text("✅ already activated")
+        await delcmd_on(chat_id)
+        await message.reply_text("🟢 activated successfully")
+    elif status == "off":
+        await delcmd_off(chat_id)
+        await message.reply_text("🔴 disabled successfully")
+    else:
+        await message.reply_text(
+            "read the /help message to know how to use this command"
+        )
+
+
+@Client.on_message(command(["volume", f"volume@{BOT_USERNAME}"]) & other_filters)
+@authorized_users_only
+async def change_volume(client, message):
+    range = message.command[1]
+    chat_id = message.chat.id
+    try:
+       await callsmusic.pytgcalls.change_volume_call(chat_id, volume=int(range))
+       await message.reply(f"🔊 **volume set to:** ```{range}%```")
+    except Exception as e:
+       await message.reply(f"**error:** {e}")
