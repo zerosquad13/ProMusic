673a1dc6ab24322bdd11f6398aa3090907ba5d79
eVEXERA <96389440+eVEXERA@users.noreply.github.com>
2022-01-01 10:16:00 +0530

    Update song.py

--- ProMusic/song.py
+++ ProMusic/song.py
@@ -19,7 +19,7 @@ from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 
 from config import BOT_USERNAME as bn
-from helpers.decorators import humanbytes
+from ProMusic.play import humanbytes
 from helpers.filters import command
 
 
